2fe02f2c7a8b3d3c9424e3dd5ff583fc6bfe2b79
22 <60903333+nini22P@users.noreply.github.com>
2022-05-14 23:54:41 +0800

    V1.3

--- popup/popup.js
+++ popup/popup.js
@@ -1,54 +1,81 @@
-function createpassword(){
-    var length = document.getElementById('range').value;
-    var symbols = document.getElementById('symbolscheckbox').checked;
+function createPassword() {
+    var length = document.getElementById('length').value;
+    var symbols = document.getElementById('symbolsCheckBox').checked;
     var str = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-    if(symbols === true) str += '!@#$%^&*()_+-=[]{}:./?';
+    if (symbols === true) str += '!@#$%^&*()_+-=[]{}:./?';
     var passwords = '';
-    for(var i = 0; i < length; i++){
-        passwords += str[Math.floor(crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1)  * str.length)];
+    for (var i = 0; i < length; i++) {
+        passwords += str[Math.floor(crypto.getRandomValues(new Uint32Array(1))[0] / (0xffffffff + 1) * str.length)];
     }
     document.getElementById('password').innerHTML = passwords;
 }
 
-function checklength(){
-    if(localStorage.getItem('length') === null) {
+function checkLocalStorage() {
+    if (localStorage.getItem('length') === null) {
         document.getElementById('range').setAttribute('value', '20');
-        document.getElementById('length').innerHTML = '20';
+        document.getElementById('length').value = '20';
     } else {
         document.getElementById('range').setAttribute('value', localStorage.getItem('length'));
-        document.getElementById('length').innerHTML = localStorage.getItem('length');
+        document.getElementById('length').value = localStorage.getItem('length');
+    }
+
+    if (localStorage.getItem('symbols') === 'true') {
+        document.getElementById('symbolsCheckBox').setAttribute('checked', 'true');
     }
 }
 
-function changelength(){
-    document.getElementById('length').innerHTML=document.getElementById('range').value;
-    createpassword();
+function changeRange() {
+    if (document.getElementById('length').value == '') {
+        document.getElementById('range').value = 0;
+    } else
+        document.getElementById('range').value = document.getElementById('length').value;
 }
 
-function savelength(){
+function changeLength() {
+    document.getElementById('length').value = document.getElementById('range').value;
+}
+
+function saveLength() {
     localStorage.setItem('length', document.getElementById('range').value);
 }
 
-function checksymbols(){
-        if(localStorage.getItem('symbols') === 'true') {
-            document.getElementById('symbolscheckbox').setAttribute('checked', 'true');
-        }
+function changeSymbols() {
+    localStorage.setItem('symbols', document.getElementById('symbolsCheckBox').checked);
 }
 
-function changesymbols(){
-    localStorage.setItem('symbols', document.getElementById('symbolscheckbox').checked);
-    createpassword();
-}
-
-function copy(){
+function copy() {
     var pw = document.getElementById('password').innerText;
     navigator.clipboard.writeText(pw);
 }
 
-checklength();
-checksymbols();
-createpassword();
-document.getElementById('range').oninput = changelength;
-document.getElementById('range').onmouseup = savelength;
-document.getElementById('symbolscheckbox').onchange = changesymbols;
-document.getElementById('copy').onclick = copy;
+function createPasswordByLength() {
+    changeRange();
+    saveLength();
+    createPassword();
+}
+
+function createPasswordByRange() {
+    changeLength();
+    saveLength();
+    createPassword();
+}
+
+function createPasswordBysymbolsCheckBox() {
+    changeSymbols();
+    createPassword();
+}
+
+function checkLanguage() {
+    if (chrome.i18n.getMessage('isEn') == 'false') {
+        document.getElementById('symbolsCheckBoxSpan').innerHTML = chrome.i18n.getMessage("symbols");
+        document.getElementById('copyButon').innerHTML = chrome.i18n.getMessage("copy");
+    }
+}
+
+checkLanguage();
+checkLocalStorage();
+createPassword();
+document.getElementById('length').oninput = createPasswordByLength;
+document.getElementById('range').oninput = createPasswordByRange;
+document.getElementById('symbolsCheckBox').onchange = createPasswordBysymbolsCheckBox;
+document.getElementById('copyButon').onclick = copy;
