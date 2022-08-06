70d5ca0642b4702a433c38877f8bb0d511835765
22 <60903333+nini22P@users.noreply.github.com>
2022-08-06 13:17:47 +0800

    Change icon.

---
+++ icons/create-icons.bat
@@ -0,0 +1,4 @@
+ffmpeg -i password-generator-lite.png -vf scale=128:128 password-generator-lite128x128.png -y
+ffmpeg -i password-generator-lite.png -vf scale=48:48 password-generator-lite48x48.png -y
+ffmpeg -i password-generator-lite.png -vf scale=32:32 password-generator-lite32x32.png -y
+ffmpeg -i password-generator-lite.png -vf scale=16:16 password-generator-lite16x16.png -y
